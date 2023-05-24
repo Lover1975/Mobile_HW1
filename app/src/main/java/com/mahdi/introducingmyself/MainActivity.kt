7ab7bbc754072214afb793cd5dbfc6d5d613313c
Lover1975 <drjoenh1983@gmail.com>
2023-05-24 10:48:58 -0700

    Night mode.

--- app/src/main/java/com/mahdi/introducingmyself/MainActivity.kt
+++ app/src/main/java/com/mahdi/introducingmyself/MainActivity.kt
@@ -2,23 +2,31 @@ package com.mahdi.introducingmyself
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.text.Layout
 import android.util.Log
 import android.widget.Button
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.json.JSONArray
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private val mHandler = Handler()
+    private var isNightMode = false
     private var stopToastCounter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.black)))
         mToastRunnable.run()
         initializeTextViewName()
         initialTextViewsIntroduction()
@@ -74,9 +82,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun initializeButtons() {
+        val textViewH1 = findViewById<TextView>(R.id.textViewF1)
+        val textViewT1 = findViewById<TextView>(R.id.textViewF1T)
+        val textViewH2 = findViewById<TextView>(R.id.textViewS2)
+        val textViewT2 = findViewById<TextView>(R.id.textViewS2T)
+        val textViewH3 = findViewById<TextView>(R.id.textViewT3)
+        val textViewT3 = findViewById<TextView>(R.id.textViewT3T)
+        val textViewH4 = findViewById<TextView>(R.id.textViewF4)
+        val textViewT4 = findViewById<TextView>(R.id.textViewF4T)
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
+        val textViewNightMode = findViewById<TextView>(R.id.textView)
+        val scrollLayout = findViewById<ScrollView>(R.id.layout_scroll)
         button1.setOnClickListener {
             val intentPhoneNumber = Intent(Intent.ACTION_DIAL)
             intentPhoneNumber.data = Uri.parse("tel:09302639518")
@@ -89,6 +108,31 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(Intent.EXTRA_SUBJECT, "subject")
             intent.putExtra(Intent.EXTRA_TEXT, "mail body")
             startActivity(Intent.createChooser(intent, ""))
+        }
+        textViewNightMode.setOnClickListener{
+            if (!isNightMode) {
+                scrollLayout.setBackgroundColor(resources.getColor(android.R.color.black))
+                textViewH1.setTextColor(R.color.white)
+                textViewH2.setTextColor(R.color.white)
+                textViewH3.setTextColor(R.color.white)
+                textViewH4.setTextColor(R.color.white)
+                textViewT1.setTextColor(R.color.white)
+                textViewT2.setTextColor(R.color.white)
+                textViewT3.setTextColor(R.color.white)
+                textViewT4.setTextColor(R.color.white)
+                isNightMode = true
+            } else {
+                scrollLayout.setBackgroundColor(resources.getColor(android.R.color.white))
+                textViewH1.setTextColor(R.color.black)
+                textViewH2.setTextColor(R.color.black)
+                textViewH3.setTextColor(R.color.black)
+                textViewH4.setTextColor(R.color.black)
+                textViewT1.setTextColor(R.color.black)
+                textViewT2.setTextColor(R.color.black)
+                textViewT3.setTextColor(R.color.black)
+                textViewT4.setTextColor(R.color.black)
+                isNightMode = false
+            }
         }
     }
 
